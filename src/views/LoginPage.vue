cf3e8c549c07199de9cdc870798cd8685d306354
Tokhirov Muhammadsodiq <tokh1rov.code@gmail.com>
2025-08-14 20:10:03 +0500

    first commit

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,24 +1,50 @@
-<script >
+<script setup>
+import { ref } from 'vue'
 import bgImage from '../assets/background_img.png'
+
+// FontAwesome import
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// Ikonni ro‘yxatga qo‘shamiz
+library.add(faUser)
+library.add(lock)
+
+// Input qiymati
+const username = ref('')
+const password = ref('')
 </script>
 
 <template>
-  <div class="bg-center flex justify-center  items-center bg-no-repeat  bg-[url('https://i.pinimg.com/1200x/d0/f3/6b/d0f36b63a7a7e23e47e6c21bf96215eb.jpg')] w-screen h-screen bg-cover  bg-gray-500  bg-blend-multiply"
+  <div
+    class="bg-center flex justify-center items-center bg-no-repeat bg-cover bg-gray-500 bg-blend-multiply w-screen h-screen"
+    :style="{ backgroundImage: `url(${bgImage})` }"
   >
-    <div class=" bg-white   w-100 rounded-2xl text-center  h-100 ">
-      <h1 class="text-green-800 uppercase py-7 text-[24px] font-medium font-serif">Login</h1>
-      <form class="max-w-sm mx-auto">
-        <div class="mb-5">
-          <input type="email" id="email" class=" border-b-4 focus:outline-none focus:border-none focus:ring-0 text-gray-900 text-sm rounded-lg  block w-full p-2.5  dark:text-white " placeholder="name@flowbite.com" required />
+    <div class="bg-white rounded-2xl text-center p-6">
+      <h1 class="text-green-800 uppercase py-4 text-[24px] font-medium font-serif">
+        Login
+      </h1>
+
+      <form class="max-w-80 mx-auto pt-4 flex flex-col gap-3">
+        <div class="mb-5 flex items-center gap-2 border-b border-green-800">
+          <FontAwesomeIcon :icon="faUser" class="text-green-800" />
+          <input
+            v-model="username"
+            type="text"
+            placeholder="Login"
+            class="flex-1 text-black placeholder:text-black focus:outline-none focus:ring-0"
+          />
+        </div>        <div class="mb-5 flex items-center gap-2 border-b border-green-800">
+        <FontAwesomeIcon :icon="lock" />
+          <input
+            v-model="password"
+            type="text"
+            placeholder="Login"
+            class="flex-1 text-black placeholder:text-black focus:outline-none focus:ring-0"
+          />
         </div>
       </form>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
-
-
-
